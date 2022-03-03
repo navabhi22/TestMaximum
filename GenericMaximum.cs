cc6192400d484e2f22a165e9e6e80a45bbe501fc
navabhi22 <navabhi22@gmail.com>
2022-03-03 23:55:23 +0530

    Abhinav Added GenericMaximum

---
+++ GenericMaximum.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMaximum
+{
+    public class GenericMaximum<T> where T : IComparable
+    {
+        public T firstValue, secondValue, thirdValue;
+
+        public GenericMaximum(T firstValue, T secondValue, T thirdValue)
+        {
+            this.firstValue = firstValue;
+            this.secondValue = secondValue;
+            this.thirdValue = thirdValue;
+        }
+        public static T MaxValue(T firstValue, T secondValue, T thirdValue)
+        {
+            if (firstValue.CompareTo(secondValue) > 0 && secondValue.CompareTo(thirdValue) > 0 ||
+                firstValue.CompareTo(secondValue) >= 0 && secondValue.CompareTo(thirdValue) > 0 ||
+                firstValue.CompareTo(secondValue) > 0 && secondValue.CompareTo(thirdValue) >= 0)
+            {
+                return firstValue;
+
+            }
+            if (secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) > 0 ||
+                secondValue.CompareTo(firstValue) >= 0 && secondValue.CompareTo(thirdValue) > 0 ||
+                secondValue.CompareTo(firstValue) > 0 && secondValue.CompareTo(thirdValue) >= 0)
+            {
+                return secondValue;
+            }
+
+            if (thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(firstValue) > 0 ||
+                thirdValue.CompareTo(firstValue) >= 0 && thirdValue.CompareTo(firstValue) > 0 ||
+                thirdValue.CompareTo(firstValue) > 0 && thirdValue.CompareTo(firstValue) >= 0)
+            {
+                return thirdValue;
+            }
+            return default;
+        }
+        public T MaxMethod()
+        {
+            T max = GenericMaximum<T>.MaxValue(this.firstValue, this.secondValue, this.thirdValue);
+            return max;
+        }
+    }
+}
