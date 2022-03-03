9598c1005aee4059213e81c97d120cb9b9d3924c
navabhi22 <navabhi22@gmail.com>
2022-03-03 23:05:08 +0530

    Abhinav Updated Program.cs

--- Program.cs
+++ Program.cs
@@ -6,7 +6,16 @@ namespace TestMaximum
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Test Maximum of 3 Variables");
+            Console.WriteLine("TC-1.1");
+            int MaxInt = MaxInteger.FindMaxInteger(78, 45, 12);
+            Console.WriteLine(MaxInt);
+            Console.WriteLine("TC-1.2");
+            MaxInt = MaxInteger.FindMaxInteger(23, 38, 12);
+            Console.WriteLine(MaxInt);
+            Console.WriteLine("TC-1.3");
+            MaxInt = MaxInteger.FindMaxInteger(23, 45, 66);
+            Console.WriteLine(MaxInt);
+
         }
     }
 }
