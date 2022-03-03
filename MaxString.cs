67f0b8f1ec07dfbaaf8df834e9d6ba9ee5d77487
navabhi22 <navabhi22@gmail.com>
2022-03-03 23:22:53 +0530

    Abhinav Added MaxString

---
+++ MaxString.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMaximum
+{
+    public class MaxString
+    {
+        public static string FindMaxString(string String1, string String2, string String3)
+        {
+            if (String1.CompareTo(String2) > 0 && String2.CompareTo(String3) > 0 ||
+                String1.CompareTo(String2) >= 0 && String2.CompareTo(String3) > 0 ||
+                String1.CompareTo(String2) > 0 && String2.CompareTo(String3) >= 0)
+            {
+                return String1;
+
+            }
+            if (String2.CompareTo(String1) > 0 && String2.CompareTo(String3) > 0 ||
+                String2.CompareTo(String1) >= 0 && String2.CompareTo(String3) > 0 ||
+                String2.CompareTo(String1) > 0 && String2.CompareTo(String3) >= 0)
+            {
+                return String2;
+            }
+
+            if (String3.CompareTo(String1) > 0 && String3.CompareTo(String1) > 0 ||
+                String3.CompareTo(String1) >= 0 && String3.CompareTo(String1) > 0 ||
+                String3.CompareTo(String1) > 0 && String3.CompareTo(String1) >= 0)
+            {
+                return String3;
+            }
+            return String1;
+        }
+    }
+}
