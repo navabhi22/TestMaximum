6da19ce71191e908af87e15db9ef7a47c9041010
navabhi22 <navabhi22@gmail.com>
2022-03-03 23:22:20 +0530

    Abhinav Updated Program.cs

--- Program.cs
+++ Program.cs
@@ -7,14 +7,14 @@ namespace TestMaximum
         static void Main(string[] args)
         {
             Console.WriteLine("TC-1.1");
-            double MaximumFloat = MaxFloat.FindMaxFloat(24.89, 24.877, 23.355);
-            Console.WriteLine(MaximumFloat);
+            string MaximumString = MaxString.FindMaxString("hello", "hi", "hey");
+            Console.WriteLine(MaximumString);
             Console.WriteLine("TC-1.2");
-            MaximumFloat = MaxFloat.FindMaxFloat(19.459, 19.877, 19.002);
-            Console.WriteLine(MaximumFloat);
+            MaximumString = MaxString.FindMaxString("good morning", "good aternoon", "good night");
+            Console.WriteLine(MaximumString);
             Console.WriteLine("TC-1.3");
-            MaximumFloat = MaxFloat.FindMaxFloat(19.959, 19.377, 20.002);
-            Console.WriteLine(MaximumFloat);
+            MaximumString = MaxString.FindMaxString("bye", "good bye", "take care");
+            Console.WriteLine(MaximumString);
         }
     }
 }
