3aa338f6d08abfa4d8cb97497d8ff8c06ec18e94
navabhi22 <navabhi22@gmail.com>
2022-03-03 23:02:23 +0530

    Greeting message for Test Maximum of 3 variable

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestMaximum
+{
+    internal class TestMaximum
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test Maximum of 3 Variables");
+        }
+    }
+}
