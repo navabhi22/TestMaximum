6445b9cf1ec948c7dd054636b87deee2328c2ba1
navabhi22 <navabhi22@gmail.com>
2022-03-04 00:44:03 +0530

    Abhinav Added MaxBySort

---
+++ MaxBySort.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMaximum
+{
+    internal class MaxBySort<T> where T : IComparable
+    {
+        public T[] value;
+        public MaxBySort(T[] value)
+        {
+            this.value = value;
+        }
+        public T[] Sort(T[] values)
+        {
+            Array.Sort(values);
+            return values;
+        }
+        public T MaxValue(params T[] values)
+        {
+            var sorted_values = Sort(values);
+            return sorted_values[^1];
+        }
+        public T MaxMethod()
+        {
+            var max = MaxValue(this.value);
+            return max;
+        }
+        public void PrintMaxValue()
+        {
+            var max = MaxValue(this.value);
+            Console.WriteLine("Maximum Value is " + max);
+        }
+    }
+}
