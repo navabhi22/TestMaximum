f124d9e2ed0b0ff548e3807ef9a975629826ecb2
navabhi22 <navabhi22@gmail.com>
2022-03-03 23:14:00 +0530

    Abhinav Added MaxFloat

---
+++ MaxFloat.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestMaximum
+{
+    public class MaxFloat
+    {
+        /// <summary>
+        /// Finds the maximum float.
+        /// </summary>
+        /// <param name="num1">The num1.</param>
+        /// <param name="num2">The num2.</param>
+        /// <param name="num3">The num3.</param>
+        /// <returns></returns>
+        public static double FindMaxFloat(double num1, double num2, double num3)
+        {
+            if (num1.CompareTo(num2) > 0 && num2.CompareTo(num3) > 0 ||
+                num1.CompareTo(num2) >= 0 && num2.CompareTo(num3) > 0 ||
+                num1.CompareTo(num2) > 0 && num2.CompareTo(num3) >= 0)
+            {
+                return num1;
+
+            }
+            if (num2.CompareTo(num1) > 0 && num2.CompareTo(num3) > 0 ||
+                num2.CompareTo(num1) >= 0 && num2.CompareTo(num3) > 0 ||
+                num2.CompareTo(num1) > 0 && num2.CompareTo(num3) >= 0)
+            {
+                return num2;
+            }
+
+            if (num3.CompareTo(num1) > 0 && num3.CompareTo(num1) > 0 ||
+                num3.CompareTo(num1) >= 0 && num3.CompareTo(num1) > 0 ||
+                num3.CompareTo(num1) > 0 && num3.CompareTo(num1) >= 0)
+            {
+                return num3;
+            }
+            return num1;
+        }
+    }
+}
